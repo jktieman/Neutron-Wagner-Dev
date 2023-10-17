de936546b7ef69b1ba175686495825e4bbcbb3f0
James Tieman <jktieman@tiemanworld.com>
2023-10-17 14:57:34 -0500

    Cleanup Usings

--- AlliedPostOffice/SendEmail.cs
+++ AlliedPostOffice/SendEmail.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Mail;
-using System.Text;
 using AlliedPostOffice.Concrete;
 using NeutronEvents;
 
