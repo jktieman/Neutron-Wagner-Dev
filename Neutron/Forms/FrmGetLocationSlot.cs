5c8bbf06b1f787cd6d38ce22e714091de574ab77
James Tieman <jktieman@gmail.com>
2025-01-14 05:51:27 -0600

    Add FrmGetLocationSlot form to handle location slot input
    Introduce FrmGetLocationSlot class in Neutron.Forms namespace.
    - Import necessary namespaces including NeutronData.ModelViews.
    - Add partial FrmGetLocationSlot class inheriting from Form.
    - Add private readonly BindingSource field for data source.
    - Add public fields SlotNumber and Rec for user input and record.
    - Implement constructor to initialize form and set data source.
    - Add ButtonCancel_Click method to handle cancel button event.
    - Add ButtonOk_Click method to handle OK button event and validate input.

---
+++ Neutron/Forms/FrmGetLocationSlot.cs
@@ -0,0 +1,51 @@
+﻿using NeutronData.ModelViews;
+using SlotNameFactory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neutron.Forms
+{
+    public partial class FrmGetLocationSlot : Form
+    {
+        private readonly BindingSource _newLocationBindingSource;
+        public string SlotNumber = string.Empty;
+        public LocationView Rec;
+        public FrmGetLocationSlot(BindingSource newLocationBindingSource)
+        {
+            InitializeComponent();
+            _newLocationBindingSource = newLocationBindingSource;            
+            TextBoxSlotNumber.Focus();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            SlotNumber = TextBoxSlotNumber.Text.Trim().ToUpper();
+
+            // get a record from _newLocationBindingSource that matches the slot
+            Rec = _newLocationBindingSource.List.Cast<LocationView>().FirstOrDefault(r => r.Slot.ToUpper() == SlotNumber);
+            if (Rec == null)
+            {
+                MessageBox.Show("No record found for the specified slot.");
+                TextBoxSlotNumber.Focus();
+                TextBoxSlotNumber.SelectAll();
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
