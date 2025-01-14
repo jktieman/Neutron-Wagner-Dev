9a5d08223028955f78a3e1d9ff44dec216aadf81
James Tieman <jktieman@gmail.com>
2025-01-14 06:09:09 -0600

    Add LoadingForm class to display a loading screen
    Introduce LoadingForm class in Neutron.Forms namespace.
    - Add necessary using directives.
    - Define LoadingForm class inheriting from Form.
    - Configure form properties: title, size, position, border style, and topmost.
    - Add a centered "Loading..." label to the form.

---
+++ Neutron/Forms/LoadingForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neutron.Forms
+{
+    public class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            this.Text = "Please Wait";
+            this.Size = new System.Drawing.Size(200, 100);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.ControlBox = false; // Disable close button
+            this.TopMost = true;
+            var loadingLabel = new Label
+            {
+                Text = "Loading...",
+                AutoSize = true,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(loadingLabel);
+        }
+    }
+}
+
