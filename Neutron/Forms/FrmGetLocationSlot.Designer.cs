48f9f73559b9e2062dc28a260a929ad95f4a1310
James Tieman <jktieman@gmail.com>
2025-01-14 05:51:39 -0600

    Add FrmGetLocationSlot form to capture slot number
    Introduce FrmGetLocationSlot form in Neutron.Forms namespace.
    Includes TextBoxSlotNumber, LabelSlotNumber, ButtonOk, and ButtonCancel controls.
    Set form properties for default buttons, fixed dialog style, and centering.

---
+++ Neutron/Forms/FrmGetLocationSlot.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Neutron.Forms
+{
+    partial class FrmGetLocationSlot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxSlotNumber = new System.Windows.Forms.TextBox();
+            this.LabelSlotNumber = new System.Windows.Forms.Label();
+            this.ButtonOk = new System.Windows.Forms.Button();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBoxSlotNumber
+            // 
+            this.TextBoxSlotNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxSlotNumber.Location = new System.Drawing.Point(73, 76);
+            this.TextBoxSlotNumber.Name = "TextBoxSlotNumber";
+            this.TextBoxSlotNumber.Size = new System.Drawing.Size(552, 143);
+            this.TextBoxSlotNumber.TabIndex = 0;
+            this.TextBoxSlotNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LabelSlotNumber
+            // 
+            this.LabelSlotNumber.AutoSize = true;
+            this.LabelSlotNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSlotNumber.Location = new System.Drawing.Point(254, 32);
+            this.LabelSlotNumber.Name = "LabelSlotNumber";
+            this.LabelSlotNumber.Size = new System.Drawing.Size(182, 32);
+            this.LabelSlotNumber.TabIndex = 3;
+            this.LabelSlotNumber.Text = "Slot Number";
+            // 
+            // ButtonOk
+            // 
+            this.ButtonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonOk.Location = new System.Drawing.Point(327, 245);
+            this.ButtonOk.Name = "ButtonOk";
+            this.ButtonOk.Size = new System.Drawing.Size(135, 42);
+            this.ButtonOk.TabIndex = 1;
+            this.ButtonOk.Text = "Ok";
+            this.ButtonOk.UseVisualStyleBackColor = true;
+            this.ButtonOk.Click += new System.EventHandler(this.ButtonOk_Click);
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonCancel.Location = new System.Drawing.Point(478, 245);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(135, 42);
+            this.ButtonCancel.TabIndex = 2;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // FrmGetLocationSlot
+            // 
+            this.AcceptButton = this.ButtonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ButtonCancel;
+            this.ClientSize = new System.Drawing.Size(691, 397);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.ButtonOk);
+            this.Controls.Add(this.LabelSlotNumber);
+            this.Controls.Add(this.TextBoxSlotNumber);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FrmGetLocationSlot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot Number Entry Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxSlotNumber;
+        private System.Windows.Forms.Label LabelSlotNumber;
+        private System.Windows.Forms.Button ButtonOk;
+        private System.Windows.Forms.Button ButtonCancel;
+    }
+}
