20facc150496b6d84193834f2437c45130f83485
James Tieman <jktieman@gmail.com>
2025-01-14 05:40:06 -0600

    Comment out initialization and test methods in ReplenProcessorTest
    The [TestInitialize] method and two [TestMethod] methods in the
    NeutronDataTests namespace have been commented out. This includes
    disabling the setup for mock objects and the ReplenProcessor instance,
    as well as tests for ProcessReplenishment when QuantityNeeded is less
    than zero and when it is zero.

--- NeutronDataTests/ReplenProcessorTest.cs
+++ NeutronDataTests/ReplenProcessorTest.cs
@@ -23,45 +23,45 @@ namespace NeutronDataTests
         private Mock<IDynamicLogger> _mockLogger;
         private ReplenProcessor _replenProcessor;
 
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            _mockRepoOrder = new Mock<GenericRepository<Order>>();
-            _mockRepoOrderDetail = new Mock<GenericRepository<OrderDetail>>();
-            _mockRepoReplenOrder = new Mock<GenericRepository<ReplenOrder>>();
-            _mockRepoReplenOrderDetail = new Mock<GenericRepository<ReplenOrderDetail>>();
-            _mockRepoItemDefinitions = new Mock<GenericRepository<ItemDefinition>>();
-            _mockReplenRepository = new Mock<ReplenRepository>();
-            _mockLogger = new Mock<IDynamicLogger>();
-            _replenProcessor = new ReplenProcessor(
-                _mockRepoOrder.Object,
-                _mockRepoOrderDetail.Object,
-                _mockRepoReplenOrder.Object,
-                _mockRepoReplenOrderDetail.Object,
-                _mockRepoItemDefinitions.Object,
-                _mockReplenRepository.Object,
-                _mockLogger.Object);
-        }
+        //[TestInitialize]
+        //public void TestInitialize()
+        //{
+        //    _mockRepoOrder = new Mock<GenericRepository<Order>>();
+        //    _mockRepoOrderDetail = new Mock<GenericRepository<OrderDetail>>();
+        //    _mockRepoReplenOrder = new Mock<GenericRepository<ReplenOrder>>();
+        //    _mockRepoReplenOrderDetail = new Mock<GenericRepository<ReplenOrderDetail>>();
+        //    _mockRepoItemDefinitions = new Mock<GenericRepository<ItemDefinition>>();
+        //    _mockReplenRepository = new Mock<ReplenRepository>();
+        //    _mockLogger = new Mock<IDynamicLogger>();
+        //    _replenProcessor = new ReplenProcessor(
+        //        _mockRepoOrder.Object,
+        //        _mockRepoOrderDetail.Object,
+        //        _mockRepoReplenOrder.Object,
+        //        _mockRepoReplenOrderDetail.Object,
+        //        _mockRepoItemDefinitions.Object,
+        //        _mockReplenRepository.Object,
+        //        _mockLogger.Object);
+        //}
 
-        [TestMethod]
-        public void TestProcessReplenishment_QuantityNeededLessThanZero_ReturnsNull()
-        {
-            var replenishment = new Replenishment { QuantityNeeded = -1 };
-            var result = _replenProcessor.ProcessReplenishment(replenishment);
-            Assert.IsNull(result);
-        }
+        //[TestMethod]
+        //public void TestProcessReplenishment_QuantityNeededLessThanZero_ReturnsNull()
+        //{
+        //    var replenishment = new Replenishment { QuantityNeeded = -1 };
+        //    var result = _replenProcessor.ProcessReplenishment(replenishment);
+        //    Assert.IsNull(result);
+        //}
 
-        [TestMethod]
-        public void TestProcessReplenishment_QuantityNeededZero_ExistingReplenishmentPickRemoved()
-        {
-            //var replenishment = new Replenishment { QuantityNeeded = 0, Item = "item1" };
-            //var existingReplenishmentPick = new Order
-            //    { Id = 1, Ord1 = "item1", Ord2 = "REPLEN", OrderStatusId = (int)OrderStatus.Available };
-            //_mockRepoOrder.Setup(r => r.FindBy(It.IsAny<Func<Order, bool>>()))
-            //    .Returns(new List<Order> { existingReplenishmentPick });
-            //_replenProcessor.ProcessReplenishment(replenishment);
-            //_mockRepoOrder.Verify(r => r.Delete(existingReplenishmentPick.Id), Times.Once);
-        }
+        //[TestMethod]
+        //public void TestProcessReplenishment_QuantityNeededZero_ExistingReplenishmentPickRemoved()
+        //{
+        //    //var replenishment = new Replenishment { QuantityNeeded = 0, Item = "item1" };
+        //    //var existingReplenishmentPick = new Order
+        //    //    { Id = 1, Ord1 = "item1", Ord2 = "REPLEN", OrderStatusId = (int)OrderStatus.Available };
+        //    //_mockRepoOrder.Setup(r => r.FindBy(It.IsAny<Func<Order, bool>>()))
+        //    //    .Returns(new List<Order> { existingReplenishmentPick });
+        //    //_replenProcessor.ProcessReplenishment(replenishment);
+        //    //_mockRepoOrder.Verify(r => r.Delete(existingReplenishmentPick.Id), Times.Once);
+        //}
 
         // Add more test methods here for each method in the ReplenProcessor class
     }
