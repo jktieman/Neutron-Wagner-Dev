61bb6294c06599d64503eeacab81e261be765a2c
James Tieman <jktieman@gmail.com>
2025-01-14 06:06:23 -0600

    Add FrmTaskNumber form for task number selection
    Introduce FrmTaskNumber class in Neutron.Forms namespace to allow
    users to select a task number from a provided list. The form includes
    a TaskNumber property, a constructor to initialize the form and set
    the ComboBox data source, and an event handler for the OK button to
    set the TaskNumber and close the form.

---
+++ Neutron/Forms/FrmTaskNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neutron.Forms
+{
+    public partial class FrmTaskNumber : Form
+    {
+        public string TaskNumber { get; set; }
+        public FrmTaskNumber(List<string> taskNumbers)
+        {
+            InitializeComponent();
+            // set the combobox data source
+            this.ComboBoxTaskNumbers.DataSource = taskNumbers;
+
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            TaskNumber = ComboBoxTaskNumbers.SelectedItem.ToString();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
