aee9471ed8da1e90809488fe971dc1008d05e696
James Tieman <jktieman@gmail.com>
2025-08-31 05:07:56 -0500

    Add SizeVelocityHeight class with size, velocity, height
    Introduced a new class `SizeVelocityHeight` in the
    `NeutronData.Models` namespace. This class includes
    three properties: `SizeCode`, `VelocityCode`, and
    `HeightCode`, to represent various aspects of size,
    velocity, and height within the application.

---
+++ NeutronData/Models/SizeVelocityHeight.cs
@@ -0,0 +1,10 @@
+﻿using NeutronData.Models.Lookups;
+
+namespace NeutronData.Models;
+public class SizeVelocityHeight
+{
+    public SizeCode SizeCode { get; set; }
+    public VelocityCode VelocityCode { get; set; }
+    public HeightCode HeightCode { get; set; }
+}
+
