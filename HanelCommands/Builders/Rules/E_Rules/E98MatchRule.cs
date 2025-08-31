3c281bccc0430b44251c2a8c0aa69ab82fef14e0
James Tieman <jktieman@gmail.com>
2025-08-31 04:42:06 -0500

    Add E98MatchRule class for command processing
    Introduces the `E98MatchRule` class in the `HanelCommands.Builders.Rules.E_Rules` namespace. This class implements the `IMatchRule` interface, providing methods to validate the "E98" command result and process command segments. The `ProcessResult` method updates device status when the command is not possible, while `CreateStatusMessage` generates detailed error messages for the lift. Additional using directives are included for necessary namespaces.

---
+++ HanelCommands/Builders/Rules/E_Rules/E98MatchRule.cs
@@ -0,0 +1,68 @@
+﻿using HanelCommands.Builders.Interfaces;
+using NeutronEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanelCommands.Builders.Rules.E_Rules
+{
+    internal class E98MatchRule : IMatchRule
+    {
+        private const string Result = "E98";
+        private const int MinCommandSegmentsLength = 4;
+        public bool IsMatch(string result)
+        {
+            return Result.Equals(result, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public void ProcessResult(string[] commandSegments, ref List<HanelDeviceStatus> hanelDeviceStatusList)
+        {
+            if (commandSegments.Length > 0 && hanelDeviceStatusList != null)
+            {
+                if (commandSegments[0].Length >= MinCommandSegmentsLength)
+                {
+                    var lift = commandSegments[0].Substring(2, 2);
+                    if (int.TryParse(lift, out int liftNumber))
+                    {
+                        var device = hanelDeviceStatusList.FirstOrDefault(r => r.DeviceNumber == liftNumber);
+                        if (device != null)
+                        {
+                            device.CommandAccepted = false;
+                            device.StatusMessage = " E98 Command is not possible.";
+                            device.StatusMessage = CreateStatusMessage(lift, commandSegments);
+                            Mediator.GetInstance().OnDisplayMessage(this, device.StatusMessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        private string CreateStatusMessage(string lift, string[] segments)
+        {
+            var sb = new StringBuilder();
+            try
+            {
+                var message = segments.FirstOrDefault(r => r.StartsWith("S"));
+                if (message == null) return string.Empty;
+                sb.AppendLine($"TOWER {lift}");
+                sb.AppendLine();
+                sb.AppendLine("Lift run error: Command is not possible.");
+                sb.AppendLine(message);
+                var x = segments.Where(s => s.StartsWith("X")).ToList();
+                foreach (var segment in x)
+                {
+                    sb.AppendLine(segment.Substring(6).Trim());
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return sb.ToString();
+
+        }
+    }
+}
