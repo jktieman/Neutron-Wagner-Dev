4a91ba4cd4e8a2bd11efa91cabcb5f23da8d867e
James Tieman <jktieman@gmail.com>
2025-08-31 05:06:36 -0500

    Add IHistoryRepository interface with InsertHistoryRecord
    Introduces the `IHistoryRepository` interface in the
    `NeutronData.Interfaces` namespace. This interface
    declares the `InsertHistoryRecord` method, which
    accepts a `History` parameter. Several using directives
    for necessary namespaces have also been added to support
    the new functionality.

---
+++ NeutronData/Interfaces/IHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using NeutronData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutronData.Interfaces;
+public interface IHistoryRepository
+{
+    void InsertHistoryRecord(History history);
+}
