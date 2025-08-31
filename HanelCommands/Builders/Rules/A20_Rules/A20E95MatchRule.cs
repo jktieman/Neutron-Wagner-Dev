bdcebea8dec96e0e33c8b09ae3862daa4d85240f
James Tieman <jktieman@gmail.com>
2025-08-31 04:41:09 -0500

    Add A20E95MatchRule class for device status processing
    Introduces the `A20E95MatchRule` class in the
    `HanelCommands.Builders.Rules.A20_Rules` namespace.
    This class implements the `IMatchRule` interface and
    provides methods for matching the "E95" result and
    processing command results. The `ProcessResult` method
    updates device statuses based on command segments,
    while `CreateStatusMessage` constructs detailed
    messages regarding lift operations. Additional using
    directives are included for necessary namespaces.

---
+++ HanelCommands/Builders/Rules/A20_Rules/A20E95MatchRule.cs
@@ -0,0 +1,73 @@
+﻿using HanelCommands.Builders.Interfaces;
+using NeutronEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanelCommands.Builders.Rules.A20_Rules
+{
+    internal class A20E95MatchRule : IMatchRule
+    {
+        private const string Result = "E95";
+        private const int MinCommandSegmentsLength = 4;
+        public bool IsMatch(string result)
+        {
+            return Result.Equals(result, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public void ProcessResult(string[] commandSegments, ref List<HanelDeviceStatus> hanelDeviceStatusList)
+        {
+            if (commandSegments.Length > 0 && hanelDeviceStatusList != null)
+            {
+                if (commandSegments[0].Length >= MinCommandSegmentsLength)
+                {
+                    var lift = commandSegments[0].Substring(2, 2);
+                    if (int.TryParse(lift, out int liftNumber))
+                    {
+                        var device = hanelDeviceStatusList.FirstOrDefault(r => r.DeviceNumber == liftNumber);
+                        if (device != null)
+                        {
+                            device.CommandSent = false;
+                            device.CommandAccepted = false;
+                            device.CommandExecuted = false;
+                            device.InMotion = false;
+                            device.StatusMessage = CreateStatusMessage(lift, commandSegments);
+                            Mediator.GetInstance().OnDisplayMessage(this, device.StatusMessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        private string CreateStatusMessage(string lift, string[] segments)
+        {
+            var sb = new StringBuilder();
+            try
+            {
+                var message = segments.FirstOrDefault(r => r.StartsWith("S"));
+                if (message == null) return string.Empty;
+                sb.AppendLine($"TOWER {lift}");
+                sb.AppendLine();
+                sb.AppendLine("Lift run was interrupted due to contents of data field are not permitted.");
+                sb.AppendLine();
+                sb.AppendLine("Press the Down Arrow/Get Tray to retrieve tray manually.");
+                
+                sb.AppendLine(message);
+                var x = segments.Where(s => s.StartsWith("X")).ToList();
+                foreach (var segment in x)
+                {
+                    sb.AppendLine(segment.Substring(6).Trim());
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return sb.ToString();
+
+        }
+    }
+}
