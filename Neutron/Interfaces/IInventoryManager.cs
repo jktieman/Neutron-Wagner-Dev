6be698f347e075f23a25967a7d6f1fe7cd6ec303
James Tieman <jktieman@gmail.com>
2025-08-31 05:00:07 -0500

    Refactor IInventoryManager to use synchronous methods
    Updated the `IInventoryManager` interface in `IInventoryManager.cs` by replacing asynchronous method signatures with synchronous counterparts. The methods `DeleteInventoryRecordAsync`, `ReleaseCheckAsync`, and `QuickReleaseCheckAsync` have been removed and replaced with `DeleteInventoryRecord`, `ReleaseCheck`, and `QuickReleaseCheck`, reflecting a shift to synchronous implementations for inventory management operations.

--- Neutron/Interfaces/IInventoryManager.cs
+++ Neutron/Interfaces/IInventoryManager.cs
@@ -1,12 +1,11 @@
-﻿using System.Threading.Tasks;
-using NeutronData.Models;
+﻿using NeutronData.Models;
 
 namespace Neutron.Interfaces
 { 
     public interface IInventoryManager
     {
-        Task<bool> DeleteInventoryRecordAsync(int invId, bool releaseOnly = false);
-        Task<bool> ReleaseCheckAsync(Inventory inventory);
-        bool QuickReleaseCheckAsync(Inventory inventory);
+        bool DeleteInventoryRecord(int invId, bool releaseOnly = false);
+        bool ReleaseCheck(Inventory inventory);
+        bool QuickReleaseCheck(Inventory inventory);
     }
 }
