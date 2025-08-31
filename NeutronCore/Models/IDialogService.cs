7f8349ebb4c716fd97c312231d92e68ee3d1554e
James Tieman <jktieman@gmail.com>
2025-08-31 05:05:36 -0500

    Add async dialog method and update interface
    Implemented a new asynchronous method `ShowAsync` in `DialogService.cs` for displaying dialogs without blocking the UI. Added two additional methods, `Show2` and `TowerError`, for different dialog configurations. Updated `IDialogService.cs` to include signatures for the new methods.

---
+++ NeutronCore/Models/IDialogService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutronCore.Models
+{
+    public interface IDialogService
+    {
+        Task<bool> ShowAsync(string title, string message, string okButtonText, string cancelButtonText);
+        bool Show2(string title, string message, string okButtonText, string cancelButtonText);
+        bool TowerError(string title, string message, string okButtonText, string cancelButtonText);
+    }
+}
