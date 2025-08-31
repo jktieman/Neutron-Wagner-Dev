6fdaa3d38e36b1924d465813ed65b6414d8c319f
James Tieman <jktieman@gmail.com>
2025-08-31 04:41:22 -0500

    Reset device state for lift commands
    This update adds functionality to reset the state of a device associated with a lift. The properties `CommandSent`, `CommandAccepted`, `CommandExecuted`, and `InMotion` are set to `false`. Additionally, the `StatusMessage` is updated using the `CreateStatusMessage` method, and the new status message is sent to a mediator for display.

--- HanelCommands/Builders/Rules/A20_Rules/A20E96MatchRule.cs
+++ HanelCommands/Builders/Rules/A20_Rules/A20E96MatchRule.cs
@@ -31,6 +31,7 @@ namespace HanelCommands.Builders.Rules.A20_Rules
                         var device = hanelDeviceStatusList.FirstOrDefault(r => r.DeviceNumber == liftNumber);
                         if (device != null)
                         {
+                            device.CommandSent = false;
                             device.CommandAccepted = false;
                             device.CommandExecuted = false;
                             device.InMotion = false;
