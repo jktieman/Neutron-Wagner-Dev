fbb53d425a6772ea17a9c845eb3325eb3b3ffe65
James Tieman <jktieman@gmail.com>
2026-09-14 12:25:30 -0500

    Add FrmPickBase class and IFrmPickBase interface
    Introduce IFrmPickBase interface with JsonData property and implement it in the new FrmPickBase class. Add necessary using directives and wrap the code in the Neutron.Forms namespace.

---
+++ Neutron/FrmPickBase.cs
@@ -0,0 +1,28 @@
+﻿// Neutron\Forms\FrmPickBase.cs
+using AlliedLogger;
+using JsonManager;
+using MetroFramework.Forms;
+using NeutronData.DataContexts;
+using NeutronData.Models;
+using NeutronData.Repositories;
+
+namespace Neutron.Forms
+{
+
+    public interface IFrmPickBase
+    {
+        IJsonData JsonData { get; }
+    }
+
+    public class FrmPickBase : IFrmPickBase
+    {
+        public readonly IJsonData JsonData;
+
+        protected FrmPickBase(IJsonData jsonData)
+        {
+            JsonData = jsonData;
+        }
+
+        IJsonData IFrmPickBase.JsonData => JsonData;
+    }
+}
